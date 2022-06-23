04c97b54456c328c7ebf6f53440c375ef50c4a00
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-06-23 12:23:49 +0100

    ✅ Add items category component

---
+++ phone_land/src/componnet/AdminDashborad/Panels/Category/list.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Item from "./items";
+import Header from "../Header";
+import { Link } from "react-router-dom";
+
+const List = ({title ,handleDeleteObject}) => {
+  return (
+    <div className="d-flex flex-column">
+      <div className="d-flex justify-content-between">
+        <Header title ={title} />
+        <Link className="my-2 btn btn-primary shadow-2 "
+          to={`/AddRepas`}>  Add Category 
+            </Link>
+      </div>
+      <div className="card-body px-0 pb-2">
+        <div className="table-responsive">
+          <table class="table align-items-center mb-0">
+            <thead>
+              <tr>
+                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">
+                  Category Name
+                </th>
+                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <Item handleDeleteObject={handleDeleteObject} />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
