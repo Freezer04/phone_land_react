4f659c1535b1583aef4b48f28673c423d5aa1530
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-06-23 12:26:14 +0100

    ✅ Add panel component

---
+++ phone_land/src/componnet/AdminDashborad/Panels/Panel.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ListProduct from "./Products/list";
+import ListBrand from "./Brand/list";
+import ListCatygory from "./Category/list";
+import ListUsers from "./Users/list"
+
+const Panel = ({ title }) => {
+  return (
+    <div>
+      <div className="container-fluid py-5 ">
+        <div className="row my-4">
+          <div className="col-lg-8 col-md-6 mb-md-0 mb-4 mx-auto">
+            <div class="card">
+              {title === "Poducts" && <ListProduct title ={title}  />}
+              {title === "Brand" && <ListBrand title ={title}  />}
+              {title === "Category" && <ListCatygory title ={title}  />}
+              {title === "Users" && <ListUsers title ={title}  />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Panel;
