c8e1d97b35b7a12db0a6430f3bc0d4f8b4ea9e3d
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-06-23 14:20:00 +0100

    🐛 remove commpntaire

--- phone_land/src/componnet/AdminDashborad/card/card.js
+++ phone_land/src/componnet/AdminDashborad/card/card.js
@@ -1,39 +1,3 @@
-// import React from 'react'
-
-// function card({title,setPanel}) {
-//   return (
-//     <div class="content-wrapper">
-//     <div class="container-xxl flex-grow-1 container-p-y">
-//     <div class="row">
-//         <div class="container-xxl flex-grow-1 container-p-y">
-//            <div class="row"> 
-//                 <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
-//                   <div class="row">
-//                     <div class="col-6 mb-4">
-//                       <div class="card">
-//                         <div class="card-body">
-//                             <span class="d-block mb-1">Payments</span>
-//                             <h3 class="card-title text-nowrap mb-2">$2,456</h3>
-//                         </div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//             </div>
-//          </div>
-//          </div>
-//     </div>
-//     </div>
-   
-             
-            
-//   )
-// }
-
-// export default card
-
-
-
 import React from 'react'
 
 function Card({title,setPanel}) {
