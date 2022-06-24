d7d9d0bf0d12b148cb7b7d8f7d3fcc753d8f8869
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-06-24 18:13:01 +0100

    ♻️ refactory code

--- phone_land/src/componnet/AdminDashborad/card/cards.js
+++ phone_land/src/componnet/AdminDashborad/card/cards.js
@@ -4,10 +4,10 @@ import Card from './card'
 function Cards({setPanel}) {
   return (
     <div>
-      <div className='container-fluid '>
+      <div className='container-fluid'>
         <div className='row mx-auto justify-content-center '>
             <Card title='Product' setPanel={setPanel} />
-            <Card title='Barnd' setPanel={setPanel} />
+            <Card title='Brand' setPanel={setPanel} />
             <Card title='Category' setPanel={setPanel} />
             <Card title='Users' setPanel={setPanel} />
         </div>
